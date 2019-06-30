9be76cd5fdfda439b8fe9c7e9829a31cb38d3c0a
kaiecon <7356184+kaiecon@users.noreply.github.com>
2019-06-30 23:44:29 +0900

    Use /Users/lily/.dotfiles

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 XDG_CONFIG_HOME=$HOME/.config
 
 [[ ! -d "$XDG_CONFIG_HOME" ]] && mkdir $XDG_CONFIG_HOME
-ln -sf ~/dotfiles/nvim $XDG_CONFIG_HOME/nvim
-ln -sf ~/dotfiles/zsh $XDG_CONFIG_HOME/zsh
-ln -sf ~/dotfiles/tmux $XDG_CONFIG_HOME/tmux
-ln -sf ~/dotfiles/zsh/.zshenv ~/.zshenv
-ln -sf ~/dotfiles/nvim/init.vim ~/.vimrc
+ln -sf ~/.dotfiles/nvim $XDG_CONFIG_HOME/nvim
+ln -sf ~/.dotfiles/zsh $XDG_CONFIG_HOME/zsh
+ln -sf ~/.dotfiles/tmux $XDG_CONFIG_HOME/tmux
+ln -sf ~/.dotfiles/zsh/.zshenv ~/.zshenv
+ln -sf ~/.dotfiles/nvim/init.vim ~/.vimrc
